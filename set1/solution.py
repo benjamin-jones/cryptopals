b26f88278a0762e3c85e1de82bf3cae8d237b4e8
Benjamin Jones <ben@benjaminjones.me>
2017-08-27 12:39:02 -0400

    Challenge 7

--- set1/solution.py
+++ set1/solution.py
@@ -2,6 +2,7 @@ import base64
 import math
 from singleXOR_decrypt import single_xor_finder
 from hamming import hamming_distance
+from Crypto.Cipher import AES
 
 def hex_string_to_bytes(str):
     return str.decode("hex")
@@ -26,22 +27,22 @@ def xor_encrypt(ptext, key):
 
 #Challenge 1
 test = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-print("\n\nChallenge 1")
+print("\n\nChallenge 1: Base64 Encoding")
 print(base64.b64encode(hex_string_to_bytes(test)))
 
 #Challenge 2
 test1 = "1c0111001f010100061a024b53535009181c"
 test2 = "686974207468652062756c6c277320657965"
-print ("\n\nChallenge 2")
+print ("\n\nChallenge 2: XOR Encrpytion")
 print(xor_string(test1, test2))
 
 #Challenge 3
-print ("\n\nChallenge 3")
+print ("\n\nChallenge 3: Single Byte XOR Encryption")
 ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 single_xor_finder(ciphertext)
 
 #Challenge 4
-print("\n\nChallenge 4")
+print("\n\nChallenge 4: Find Single Byte XOR Encryption")
 largest = 0.0
 cur_key = 0
 cur_ptext = ""
@@ -61,14 +62,14 @@ print("Ciphertext = %s" % cur_ctext)
 print("Plaintext = %s" % cur_ptext)
 
 #Challenge 5
-print("\n\nChallenge 5")
+print("\n\nChallenge 5: Repeating Key XOR Encryption")
 ptext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 key = "ICE"
 print("Encrypting %s with key %s" % (ptext, key))
 print(xor_encrypt(ptext, key))
 
 #Challenge 6
-print("\n\nChallenge 6")
+print("\n\nChallenge 6: Break Repeating Key XOR Encrpytion")
 distance = hamming_distance("this is a test","wokka wokka!!!")
 
 if distance != 37:
@@ -128,4 +129,11 @@ for block in transposed_blocks:
 
 ptext = xor_encrypt(ciphertext, key)
 print(hex_string_to_bytes(ptext))
+
+#Challenge 7
+print("\n\nChallenge 7: AES in ECB mode")
+new_ciphertext = base64.b64decode(open("./7.txt","r").read())
+
+aes = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
+print(aes.decrypt(new_ciphertext))
 
